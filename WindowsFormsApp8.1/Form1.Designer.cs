f297fbb6b8d9a9a76dcac9f9a16ff9db56a876e1
barbraa08 <101864610+barbraa08@users.noreply.github.com>
2023-08-27 17:05:31 +0700

    LAB8.1

---
+++ WindowsFormsApp8.1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp8._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbAnswer = new System.Windows.Forms.Label();
+            this.tbQuestion = new System.Windows.Forms.TextBox();
+            this.btAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbAnswer
+            // 
+            this.lbAnswer.AutoSize = true;
+            this.lbAnswer.Location = new System.Drawing.Point(347, 258);
+            this.lbAnswer.Name = "lbAnswer";
+            this.lbAnswer.Size = new System.Drawing.Size(0, 20);
+            this.lbAnswer.TabIndex = 0;
+            // 
+            // tbQuestion
+            // 
+            this.tbQuestion.Location = new System.Drawing.Point(193, 72);
+            this.tbQuestion.Multiline = true;
+            this.tbQuestion.Name = "tbQuestion";
+            this.tbQuestion.Size = new System.Drawing.Size(376, 75);
+            this.tbQuestion.TabIndex = 1;
+            // 
+            // btAnswer
+            // 
+            this.btAnswer.Location = new System.Drawing.Point(309, 171);
+            this.btAnswer.Name = "btAnswer";
+            this.btAnswer.Size = new System.Drawing.Size(135, 64);
+            this.btAnswer.TabIndex = 2;
+            this.btAnswer.Text = "Answer";
+            this.btAnswer.UseVisualStyleBackColor = true;
+            this.btAnswer.Click += new System.EventHandler(this.btAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btAnswer);
+            this.Controls.Add(this.tbQuestion);
+            this.Controls.Add(this.lbAnswer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbAnswer;
+        private System.Windows.Forms.TextBox tbQuestion;
+        private System.Windows.Forms.Button btAnswer;
+    }
+}
+
---
+++ WindowsFormsApp8.1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace WindowsFormsApp8._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double p = 0.5;
+        double r;
+        Random rand = new Random();
+        private void btAnswer_Click(object sender, EventArgs e)
+        {
+            lbAnswer.Text.Trim();
+            if (tbQuestion.Text != String.Empty)
+            {
+                r = rand.NextDouble();
+                if (r < p) lbAnswer.Text = "Yes!";
+                else lbAnswer.Text = "No!";
+            }
+            else lbAnswer.Text = "Ask me a question.";
+        }
+    }
+}
